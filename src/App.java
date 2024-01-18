4c451c18177f5d85c4afa481d8312dc8d2fc83ac
Laboratorio 109 Pc-15 <lab-109-inf-15>
2024-01-18 19:05:48 -0400

    primer commit

---
+++ src/App.java
@@ -0,0 +1,16 @@
+import java.io.PrintStream;
+
+public class App {
+    public static void main(String[] args){
+        String nombre;
+        double monto;
+        double resultado;
+
+        nombre="Nayelhi";
+        monto=475.50;
+        resultado=monto * 1.15;
+        System.out.println("Hola" + nombre + "el valor total es " + resultado);
+
+    }
+    }
+
